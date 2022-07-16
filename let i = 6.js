075777f1815b268c950609f16b3e0de2d78a2c73
jacksonslk <55043747+jacksonslk@users.noreply.github.com>
2022-07-16 20:55:33 +0530

    ending all javascript commits

---
+++ let i = 6.js
@@ -0,0 +1,22 @@
+let i = 6;
+
+let j = 1;
+
+let arr = [i, 1, 2, 3, 4];
+
+while((arr.length - 1)/2){
+
+    if(arr(j)>arr(j+1)){
+
+    arr[j+1] = arr[j] + (arr[j+1] - arr[j]);
+
+    i = i - (arr(j)-arr(j+1));
+
+ }
+ else{
+
+
+
+ }
+
+}
